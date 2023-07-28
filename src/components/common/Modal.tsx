5dbb280d7005b88c5993a866e92e1e031384f69c
ChaDongmin <cdm1263@gmail.com>
2023-07-28 18:07:42 +0900

    💄 design : #4 Modal 외부 blur 처리

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -1,23 +1,48 @@
 import { ReactNode } from "react";
 import { styled } from "styled-components";
+import { theme } from "../../styles/theme";
 
 interface IChildren {
   children: ReactNode;
 }
 
 const Modal = ({ children }: IChildren) => {
-  return <ModalLayout>{children}</ModalLayout>;
+  return (
+    <ModalWrapper>
+      <ModalLayout>{children}</ModalLayout>
+    </ModalWrapper>
+  );
 };
 
+const ModalWrapper = styled.div`
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 1;
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  background-color: rgba(0, 0, 0, 0.2);
+`;
+
 const ModalLayout = styled.div`
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  margin: auto;
   width: 500px;
   height: 500px;
   padding: 20px;
   display: flex;
-  justify-content: space-between;
   border: 1px solid;
+  position: absolute;
+  border-radius: 20px;
   flex-direction: column;
   background-color: aliceblue;
+  justify-content: space-between;
+  box-shadow: 4px 4px 1.25rem ${theme.colors.black};
 `;
 
 export default Modal;
