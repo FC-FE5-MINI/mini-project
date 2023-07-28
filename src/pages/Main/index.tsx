5ce34fcb9629c6a55c17b55c210b8b60dbb87dac
EUNJI <eunji9180@naver.com>
2023-07-28 15:02:22 +0900

    :sparkles: #7 Calendar 컴포넌트 시작

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,5 +1,15 @@
+import Calendar from "../../components/calendar";
+import styled from "styled-components";
+
+const MainContainer = styled.div`
+  margin: 3rem;
+`;
 const Main = () => {
-  return <div>main</div>;
+  return (
+    <MainContainer>
+      <Calendar />
+    </MainContainer>
+  );
 };
 
 export default Main;
