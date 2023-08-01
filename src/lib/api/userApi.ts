59df61bb01c3156524eaab34197c91a1fc02e66d
iskra17 <rayanami@naver.com>
2023-08-01 14:46:46 +0900

    :sparkles: Feat : #9 로그인 페이지 기능 구현 완료(API는 mock server)
    로그인 기능은 구현 완료. 실제 API만 적용하면 됨.

--- src/lib/api/userApi.ts
+++ src/lib/api/userApi.ts
@@ -14,3 +14,8 @@ export const signUp = async (email: string, password: string, username: string) 
   const response = await api.post("/user/join", { email, password, username });
   return response.data;
 };
+
+export const login = async (email: string, password: string) => {
+  const response = await api.post("/user/login", { email, password });
+  return response.data;
+};
