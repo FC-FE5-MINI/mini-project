c487112f9f745bcd8f96791f33b348aa0fb1bb20
ChaDongmin <cdm1263@gmail.com>
2023-08-04 00:56:54 +0900

    ✨ feat : 신청 완료 문구 추가

--- src/lib/util/constants.ts
+++ src/lib/util/constants.ts
@@ -18,6 +18,7 @@ const ORDER_STATE: { [key: string]: OrderStateType } = {
 const MODAL_MESSAGE = {
   PLEASE_ENDDATE: "연차 종료 날짜를 설정해 주세요.",
   CANCELED: "신청이 취소되었습니다.",
+  ADD_SUCCESS: "신청이 완료되었습니다.",
 };
 
 export { TAB_ADD, EVENT_TYPE, MODAL_MESSAGE, ORDER_STATE };
