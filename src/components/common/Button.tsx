87c9d9d97420bd0a8422f015f6b917f70fc89479
ChaDongmin <cdm1263@gmail.com>
2023-08-05 19:46:43 +0900

    💄 design : #4 hover 스타일 추가

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -17,41 +17,58 @@ const StyledButton = styled.button<{
   padding: 5px 10px;
   border-radius: 5px;
   border: 1px solid ${theme.colors.black};
+  transition: all 0.2s ease-in-out;
 
   // 흰배경에 녹색 버튼
   ${({ $greenLight }) =>
     $greenLight &&
     css`
-      color: ${theme.colors.green.dark};
+      color: ${theme.colors.green.main};
       background-color: ${theme.colors.white};
-      border: 1px solid ${theme.colors.green.dark};
-    `}
+      border: 1px solid ${theme.colors.green.main};
 
+      &:hover {
+        color: ${theme.colors.white};
+        background-color: ${theme.colors.green.main};
+      }
+    `}
   // 녹색배경에 흰색 버튼
-      ${({ $greenDark }) =>
+    ${({ $greenDark }) =>
     $greenDark &&
     css`
       color: ${theme.colors.white};
-      background-color: ${theme.colors.green.dark};
-      border: 1px solid ${theme.colors.green.dark};
+      background-color: ${theme.colors.green.main};
+      border: 1px solid ${theme.colors.green.main};
+
+      &:hover {
+        background-color: ${theme.colors.green.dark};
+      }
     `}
-      
-      // 흰배경에 오렌지색 버튼
-      ${({ $orangeLight }) =>
+    // 흰배경에 오렌지색 버튼
+    ${({ $orangeLight }) =>
     $orangeLight &&
     css`
       color: ${theme.colors.orange.dark};
       background-color: ${theme.colors.white};
-      border: 1px solid ${theme.colors.orange.dark};
+      border: 1px solid ${theme.colors.orange.main};
+
+      &:hover {
+        color: ${theme.colors.white};
+        background-color: ${theme.colors.orange.main};
+      }
     `}
-      
-      // 오렌지배경에 흰색 버튼
-      ${({ $orangeDark }) =>
+
+    // 오렌지배경에 흰색 버튼
+    ${({ $orangeDark }) =>
     $orangeDark &&
     css`
       color: ${theme.colors.white};
-      background-color: ${theme.colors.orange.dark};
-      border: 1px solid ${theme.colors.orange.dark};
+      background-color: ${theme.colors.orange.main};
+      border: 1px solid ${theme.colors.orange.main};
+
+      &:hover {
+        background-color: ${theme.colors.orange.dark};
+      }
     `}
 `;
 export default Button;
